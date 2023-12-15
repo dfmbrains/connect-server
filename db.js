d59980d540746af966334cfc47701daefe2898e5
jarvis <maxabdurasulov@gmail.com>
2023-12-16 00:40:59 +0800

    Implement PostgreSQL connection with Vercel deployment compatibility
    The commit includes Postgres integration with both local and Vercel deployed environments. In this update, the local and production environment variables have been configured separately. Also, updated database configuration in db.js to use environment variables. Additionally, "@vercel/postgres" package has been added to use PostgreSQL on the Vercel environment.

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production.local' : '.env.development.local'
+});
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  password: 'root',
-  host: 'localhost',
-  port: 5432,
-  database: 'connect_app'
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE
 })
 
 module.exports = pool
