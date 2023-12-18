6d6fdb06bbadaabc70806521bd542d62331ae58d
jarvis <maxabdurasulov@gmail.com>
2023-12-19 01:05:35 +0800

    Implement conditional SSL configuration in db.js
    Introduced a conditional SSL configuration based on the environment in db.js. This allows SSL settings to be applied dynamically, directly in the database pool instantiation. This improvement ensures SSL is applied appropriately, whether in a production or development environment.

--- db.js
+++ db.js
@@ -3,17 +3,20 @@ require('dotenv').config({
 });
 
 const Pool = require('pg').Pool
+
+const sslConfig = process.env.NODE_ENV === 'production' ? {
+  rejectUnauthorized: false,
+  require: true,
+  mode: 'require'
+} : null;
+
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DATABASE,
-  ssl: {
-    rejectUnauthorized: false,
-    require: true,
-    mode: 'require'
-  }
-})
+  ssl: sslConfig
+});
 
 module.exports = pool
